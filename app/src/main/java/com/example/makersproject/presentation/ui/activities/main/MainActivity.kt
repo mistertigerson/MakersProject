5e598045659e09a40daf490434ebfe591c0f0ba2
AbylovErmek <gskro705@gmail.com>
2022-03-14 23:25:37 +0600

    test

--- app/src/main/java/com/example/makersproject/presentation/ui/activities/main/MainActivity.kt
+++ app/src/main/java/com/example/makersproject/presentation/ui/activities/main/MainActivity.kt
@@ -2,10 +2,10 @@ package com.example.makersproject.presentation.ui.activities.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.makersproject.R
 import com.example.makersproject.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,22 +13,16 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private val binding: ActivityMainBinding by viewBinding()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        val appBarConfiguration = AppBarConfiguration.Builder(
-            R.id.firstRegistrationFragment, R.id.personalAreaFragment)
-            .build()
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        val navHostController = supportFragmentManager.findFragmentById(R.id.navHostFragment)
+        val navController: NavController = navHostController!!.findNavController()
         binding.bottomNav.setupWithNavController(navController)
 
-        //коментарий
+
     }
 }
