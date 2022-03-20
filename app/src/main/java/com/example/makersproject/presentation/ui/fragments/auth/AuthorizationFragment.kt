42195c8e13565d94b96e954e933b081203f822ab
AziTimur <azik00kgz@gmail.com>
2022-03-20 20:38:53 +0600

    local

--- app/src/main/java/com/example/makersproject/presentation/ui/fragments/auth/AuthorizationFragment.kt
+++ app/src/main/java/com/example/makersproject/presentation/ui/fragments/auth/AuthorizationFragment.kt
@@ -1,6 +1,7 @@
 package com.example.makersproject.presentation.ui.fragments.auth
 
 import android.app.Activity
+import android.app.Activity.RESULT_OK
 import android.app.AlertDialog
 import android.content.ContentValues.TAG
 import android.content.DialogInterface
@@ -24,7 +25,11 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthEmailException
 import com.google.firebase.auth.GoogleAuthProvider
 
 
@@ -58,7 +63,7 @@ class AuthorizationFragment : Fragment(R.layout.fragment_authorization) {
             val builder = AlertDialog.Builder(requireContext())
             builder.setTitle("Forgot password")
             val view = layoutInflater.inflate(R.layout.dialog_forget_password, null)
-            val username=view.findViewById<EditText>(R.id.etEmail)
+            val username = view.findViewById<EditText>(R.id.etEmail)
             builder.setView(view)
             builder.setPositiveButton("Reset", DialogInterface.OnClickListener { _, _ ->
                 forgetPassword(username)
@@ -68,92 +73,100 @@ class AuthorizationFragment : Fragment(R.layout.fragment_authorization) {
         }
     }
 
-    private fun forgetPassword(username:EditText) {
+    private fun forgetPassword(username: EditText) {
         if (username.text.toString().isEmpty()
         ) {
-            Toast.makeText(requireContext(),
-                "Fields cannot be empty ", Toast.LENGTH_SHORT).show()
-        }
-        if (!Patterns.EMAIL_ADDRESS.matcher(username.text.toString()).matches()){
-            return
-    }
-        auth.sendPasswordResetEmail(username.text.toString())
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Log.d(TAG, "Email sent.")
-                    Toast.makeText(requireContext(),
-                        "Password reset link sent ", Toast.LENGTH_SHORT).show()
-                }
-            }
 
-    }
-
-private fun initEditText() {
-    binding.btnSignIn.setOnClickListener {
-        if (binding.etMailSignIn.text.toString().isEmpty() || binding.etPasswordSignIn.text
-                .toString().isEmpty()
-        ) {
             Toast.makeText(requireContext(),
                 "Fields cannot be empty ", Toast.LENGTH_SHORT).show()
         } else {
-            auth.signInWithEmailAndPassword(binding.etMailSignIn.text.toString(), binding
-                .etPasswordSignIn.text.toString()).addOnCompleteListener {
-                if (it.isSuccessful) {
-                    findNavController().navigate(R.id.mainFragment)
-                } else {
-                    Toast.makeText(requireContext(),
-                        "You have some errors ", Toast.LENGTH_SHORT).show()
+            auth.sendPasswordResetEmail(username.text.toString())
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "Email sent."+task.exception!!.message.toString())
+                        Toast.makeText(requireContext(),
+                            "Password reset link sent "+task.exception!!.message.toString(), Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(
+                            requireContext(),
+                            task.exception!!.message.toString(),
+                            Toast.LENGTH_LONG).show()
+
+                    }
+                }
+        }
+
+    }
+
+    private fun initEditText() {
+        binding.btnSignIn.setOnClickListener {
+            if (binding.etMailSignIn.text.toString().isEmpty() || binding.etPasswordSignIn.text
+                    .toString().isEmpty()
+            ) {
+                Toast.makeText(requireContext(),
+                    "Fields cannot be empty ", Toast.LENGTH_SHORT).show()
+            } else {
+                auth.signInWithEmailAndPassword(binding.etMailSignIn.text.toString(), binding
+                    .etPasswordSignIn.text.toString()).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        findNavController().navigate(R.id.mainFragment)
+                    } else {
+                        Toast.makeText(requireContext(),
+                            "You have some errors ", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
-
         }
     }
-}
 
-private fun initGmail() {
-    val gso = GoogleSignInOptions
-        .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-        .requestIdToken(getString(R.string.web_client))
-        .requestEmail()
-        .build()
-    googleClient = GoogleSignIn.getClient(requireActivity(), gso)
-}
+    private fun initGmail() {
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.web_client))
+            .requestEmail()
+            .build()
+        googleClient = GoogleSignIn.getClient(requireActivity(), gso)
+    }
 
-private fun gmailSignIn() {
-    val intent = googleClient.signInIntent
-    resultLauncher.launch(intent)
-}
+    private fun gmailSignIn() {
+        val intent = googleClient.signInIntent
+        resultLauncher.launch(intent)
+    }
 
-private val resultLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
-    ActivityResultContracts.StartActivityForResult(),
-    ActivityResultCallback {
-        if (it.resultCode == Activity.RESULT_OK) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+    private val resultLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult(),
+        ActivityResultCallback {
+            val task: Task<GoogleSignInAccount> =
+                GoogleSignIn.getSignedInAccountFromIntent(it.data)
+            if (it.resultCode == RESULT_OK) {
+                try {
+                    val account = task.getResult(ApiException::class.java)!!
+                    signWithGmail(account)
+                } catch (e: ApiException) {
+                    e.printStackTrace()
 
-            try {
-                val account = task.getResult(ApiException::class.java)!!
-                signWithGmail(account)
-            } catch (e: ApiException) {
-                e.printStackTrace()
+                }
             }
-        }
-    })
+        })
 
-private fun signWithGmail(account: GoogleSignInAccount) {
-    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-    FirebaseAuth.getInstance().signInWithCredential(credential)
-        .addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                close()
-            } else {
-                Toast.makeText(requireContext(),
-                    "AuthError ", Toast.LENGTH_SHORT).show()
-            }
-        }
-}
+    private fun signWithGmail(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+            .addOnCompleteListener(OnCompleteListener<AuthResult>() { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG,"Google signIn done")
+                    close()
+                    findNavController().navigate(R.id.mainFragment)
+                } else {
+                    Log.e(TAG,"Google signIn error" + task.exception?.message.toString())
 
-private fun close() {
-    val navController = findNavController()
-    navController.navigateUp()
-}
+                    Toast.makeText(requireContext(),
+                        "Error ", Toast.LENGTH_SHORT).show()
+                }
+            })
+    }
+    private fun close() {
+        val navController = findNavController()
+        navController.navigate(R.id.mainFragment)
+    }
 }
