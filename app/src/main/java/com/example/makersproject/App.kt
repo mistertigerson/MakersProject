ceb08affe9639e32cc7146a5cfc4974175264a82
JKramer07 <turanraiga@rocketmail.com>
2022-03-20 19:31:51 +0600

    fixed App

--- app/src/main/java/com/example/makersproject/App.kt
+++ app/src/main/java/com/example/makersproject/App.kt
@@ -2,6 +2,7 @@ package com.example.makersproject
 
 import android.app.Application
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 //import androidx.room.Room
 //import com.example.makersproject.data.local.AppDataBase
 import dagger.hilt.android.HiltAndroidApp
@@ -13,9 +14,11 @@ class App : Application() {
         super.onCreate()
 
         fbAuth = FirebaseAuth.getInstance()
+        fireStore = FirebaseFirestore.getInstance()
     }
 
     companion object{
         lateinit var fbAuth: FirebaseAuth
+        lateinit var fireStore: FirebaseFirestore
     }
 }
